42416364f2f517f82c2d93e9d1c3251a1048dc79
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-14 11:03:16 +0200

    Initial Commit

---
+++ SignDecoder/SignDecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignDecoderApp.swift
+//  SignDecoder
+//
+//  Created by Ngoni Katsidzira  on 14/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct SignDecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
