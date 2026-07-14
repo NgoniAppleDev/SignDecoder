a1282d86cc47dd98d7ebcfcdc567e39bb5c1e690
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-14 11:55:14 +0200

    feat: create a text recognizer
    Locate and identify text in an image using the Vision machine learning framework.

---
+++ SignDecoder/TextRecognizer.swift
@@ -0,0 +1,33 @@
+//
+//  TextRecognizer.swift
+//  SignDecoder
+//
+//  Created by Ngoni Katsidzira  on 14/7/2026.
+//
+
+import SwiftUI
+import Vision
+
+struct TextRecognizer {
+    
+    var recognizedText = ""
+    var observations: [RecognizedTextObservation] = []
+    
+    init(imageResource: ImageResource) async {
+        var request = RecognizeTextRequest()
+        
+        let image = UIImage(resource: imageResource)
+        
+        if let imageData = image.pngData(), let results = try? await request.perform(on: imageData) {
+            observations = results
+        }
+        
+        for observation in observations {
+            let candidate = observation.topCandidates(1)
+            
+            if let observedText = candidate.first?.string {
+                recognizedText += "\(observedText)"
+            }
+        }
+    }
+}
