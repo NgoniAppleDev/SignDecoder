1f75545de9bd11a48baf80c8e88533cfbceda0eb
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-14 11:33:39 +0200

    feat: translate text to another language

---
+++ SignDecoder/TranslationView.swift
@@ -0,0 +1,42 @@
+//
+//  TranslationView.swift
+//  SignDecoder
+//
+//  Created by Ngoni Katsidzira  on 14/7/2026.
+//
+
+import SwiftUI
+import Translation
+
+struct TranslationView: View {
+    
+    var text: String
+    @State private var showingTranslation = false
+    
+    var body: some View {
+        VStack {
+            Text("Identified Text")
+                .font(.subheadline.bold())
+                .textCase(.uppercase)
+                .foregroundStyle(.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+            
+            Text("Caution, falling rocks")
+                .frame(maxWidth: .infinity, maxHeight: 50, alignment: .topLeading)
+                .padding()
+                .background(Color(white: 0.9))
+                .translationPresentation(isPresented: $showingTranslation, text: text)
+            
+            Button {
+                showingTranslation = true
+            } label: {
+                Text("Translate")
+            }
+        }
+    }
+}
+
+#Preview {
+    TranslationView(text: "Caution, falling rocks")
+}
